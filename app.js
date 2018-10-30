ecdad5e6ec28acac3f44f2c434eecdb090c431ed
刘专 <liuzhuankyzy@gmail.com>
2018-10-30 11:50:56 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,22 +1,26 @@
-//app.js
 App({
   onLaunch: function () {
-    //调用API从本地缓存中获取数据
-    var logs = wx.getStorageSync('logs') || []
+    // 调用API从本地缓存中获取数据
+    var logs = my.getStorageSync({ key: 'logs' }).data || []
     logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
+    my.setStorageSync({
+      key: 'logs', 
+      data: logs
+    });
   },
+  
   getUserInfo:function(cb){
     var that = this
-    if(this.globalData.userInfo){
+    if (this.globalData.userInfo) {
       typeof cb == "function" && cb(this.globalData.userInfo)
-    }else{
+    } else {
       //调用登录接口
-      wx.login({
+      my.getAuthCode({
+        scopes: "auth_user",
         success: function () {
-          wx.getUserInfo({
+          my.getAuthUserInfo({
             success: function (res) {
-              that.globalData.userInfo = res.userInfo
+              that.globalData.userInfo = res;
               typeof cb == "function" && cb(that.globalData.userInfo)
             }
           })
@@ -24,6 +28,7 @@ App({
       })
     }
   },
+  
   globalData:{
     userInfo:null
   }
